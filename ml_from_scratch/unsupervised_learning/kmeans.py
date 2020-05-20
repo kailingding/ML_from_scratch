d8eb1a9efec9c2f7d0f73614ad755b82314bbc8b
Kailing Ding <markdingkl@gmail.com>
2020-05-19 20:49:10 -0700

    add kmeans

---
+++ ml_from_scratch/unsupervised_learning/kmeans.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class KMeans:
+    def __init__(self):
+    	"""
+			WIP: KMeans class
+    	"""
+        self.WIP = 0
+
+    def fit(self, X, y):
+        return self.WIP
+
+    def predict(self, X):
+        return self.WIP

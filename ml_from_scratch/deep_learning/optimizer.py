6102e595aeb0f0581bb2ad2f525cf0420b987c0d
Kailing Ding <markdingkl@gmail.com>
2020-06-18 12:42:41 -0700

    added more

---
+++ ml_from_scratch/deep_learning/optimizer.py
@@ -0,0 +1,44 @@
+import numpy
+
+
+class SGD:
+	'''
+		SGD pytorch code snippet:
+			train_loader = DataLoader(dataset=dataset, batch_size=1)
+			for epoch in range(n_epochs):
+				for x_batch, y_batch in train_loader:
+					train(...)
+					validate(...)
+
+		Mini-batch GD:
+			# X.shape = (100, 4)
+			train_loader = DataLoader(dataset=dataset, batch_size=5) # 5 instances
+			for epoch in range(n_epochs):
+				for x_batch, y_batch in train_loader:
+					train(...)
+					validate(...)
+
+	'''
+    def __init__(self, lr=0.01, momentum=0):
+        self.lr = lr
+        self.momentum = momentum
+        self.updated_w = None
+
+    def step(self, params, grad_wrt_w):
+        if self.updated_w is None:
+            self.updated_w = np.zeros(np.shape(params))
+
+        # use momentun if set
+        self.updated_w = self.momentum * self.updated_w + (1 - self.momentum) * grad_wrt_w
+
+        # gradient descent
+        return params - self.lr * self.updated_w
+
+
+
+class Adagrad:
+	def __init__(self):
+		pass
+
+	def step(self):
+		pass
--- ml_from_scratch/unsupervised_learning/gaussian_mixture.py
+++ ml_from_scratch/unsupervised_learning/gaussian_mixture.py
@@ -1,1 +1,130 @@
 import numpy as np
+import pandas as pd
+from kmeans import KMeans
+
+
+def gaussian(X, mu, cov):
+    """ 
+        Fucntion to create mixtures using the Given matrix X, given covariance and given mu
+
+        Return:
+        transformed x.
+    """
+    # X should be matirx-like
+    n = X.shape[1]
+    diff = (X - mu).T
+    return np.diagonal(1 / ((2 * np.pi) ** (n / 2) * np.linalg.det(cov) ** 0.5
+                            ) * np.exp(-0.5 * np.dot(np.dot(
+                                diff.T, np.linalg.inv(cov)), diff))).reshape(-1, 1)
+
+
+def initialize_clusters(X, n_clusters):
+    """ 
+        Initialize the clusters by storing the information in the data matrix X into the clusters
+
+        Parameter:
+            X: Input feature matrix
+            n_clusters: Number of clusters we are trying to classify
+
+        Return:
+            cluster: List of clusters. Each cluster center is calculated by the KMeans algorithm above.
+    """
+    clusters = []
+    index = np.arange(X.shape[0])
+
+    # We use the KMeans centroids to initialise the GMM
+
+    kmeans = KMeans().fit(X)
+    mu_k = kmeans.centers
+
+    for i in range(n_clusters):
+        clusters.append({
+            'w_k': 1.0 / n_clusters,
+            'mu_k': mu_k[i],
+            'cov_k': np.identity(X.shape[1], dtype=np.float64)
+        })
+
+    return clusters
+
+
+def expectation_step(X, clusters):
+    """ 
+        "E-Step" for the GM algorithm
+
+        Parameter:
+            X: Input feature matrix
+            clusters: List of clusters
+    """
+    totals = np.zeros((X.shape[0], 1), dtype=np.float64)
+
+    for cluster in clusters:
+        w_k = cluster['w_k']
+        mu_k = cluster['mu_k']
+        cov_k = cluster['cov_k']
+
+        # Calculate the numerator part of the cluster posterior
+        posterior = w_k * gaussian(X, mu_k, cov_k)
+
+        totals = np.sum([cluster['w_k'] * gaussian(X, cluster['mu_k'], cluster['cov_k'])
+                         for cluster in clusters], axis=0)
+
+        cluster['posterior'] = posterior
+        cluster['totals'] = totals
+
+    for cluster in clusters:
+        # Calculate the cluster posterior using totals
+        cluster['posterior'] = cluster['posterior'] / cluster['totals']
+
+
+def maximization_step(X, clusters):
+    """ 
+        "M-Step" for the GM algorithm
+
+        Parameter:
+            X: Input feature matrix
+            clusters: List of clusters
+    """
+    N = float(X.shape[0])
+
+    for cluster in clusters:
+        posterior = cluster['posterior']
+        cov_k = np.zeros((X.shape[1], X.shape[1]))
+
+        # Calculate the new cluster data
+        N_k = np.sum(posterior, axis=0)
+        w_k = N_k / N
+        mu_k = (posterior * X).sum(axis=0) / N_k
+
+        # for j in range(X.shape[0]):
+        cov_k = (X - mu_k).T.dot((X - mu_k) * posterior)
+
+        cov_k /= N_k
+
+        cluster['w_k'] = w_k
+        cluster['mu_k'] = mu_k
+        cluster['cov_k'] = cov_k
+
+
+def get_likelihood(X, clusters):
+    likelihood = []
+    sample_likelihoods = np.log(np.array([cluster['totals'] for cluster in clusters]))
+    return np.sum(sample_likelihoods), sample_likelihoods
+
+
+def train_gmm(X, n_clusters, n_epochs):
+    clusters = initialize_clusters(X, n_clusters)
+    likelihoods = np.zeros((n_epochs, ))
+    scores = np.zeros((X.shape[0], n_clusters))
+
+    for i in range(n_epochs):
+
+        expectation_step(X, clusters)
+        maximization_step(X, clusters)
+
+        likelihood, sample_likelihoods = get_likelihood(X, clusters)
+        likelihoods[i] = likelihood
+
+    for i, cluster in enumerate(clusters):
+        scores[:, i] = np.log(cluster['w_k']).reshape(-1)
+
+    return clusters, likelihoods, scores, sample_likelihoods
